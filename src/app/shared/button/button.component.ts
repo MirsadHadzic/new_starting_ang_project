02621a15f2f5267263337a87cde6baeaff903ebd
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-05-08 21:34:46 +0200

    migrating all the css staff

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  // for component and ng selectors, i mean a is for anchor tags
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.css',
 })
 export class ButtonComponent {
 
