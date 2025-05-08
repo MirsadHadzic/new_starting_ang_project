0b6fe2436ef3ebdad69a2d027c65de5eb5eef346
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-05-08 21:47:48 +0200

    migrated a few more css styles

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,14 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  // disable encapsulation so css styles can work
+  encapsulation: ViewEncapsulation.None
+  //.emulated is the default one
 })
 export class ControlComponent {
   label = input.required<string>();
