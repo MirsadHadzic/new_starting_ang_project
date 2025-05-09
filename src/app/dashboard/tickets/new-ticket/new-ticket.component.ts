38d8ff1c39f1f2206c2bacc94a69bace626d2794
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-05-09 22:41:58 +0200

    committingg a this kako se zove, uglabnom novi form reset, novi nacin i tkao

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -11,9 +11,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(titleElement: HTMLInputElement) {
-    // there's also a console.dir for logging
-    const enteredTitle = titleElement.value;
-    console.log(enteredTitle);
+  // onSubmit(titleElement: HTMLInputElement) {
+  //   // there's also a console.dir for logging
+  //   const enteredTitle = titleElement.value;
+  //   console.log(enteredTitle);
+
+  // }
+  // onSubmit(title: string, ticketText: string)
+  // {
+  //   const enteredTitle = titleElement.value;
+  //   console.log('ENTERED TITLE: ' + enteredTitle);
+  // }
+
+    // ? needs to be here cause this property here will not be populated with a value instantly
+    @ViewChild('form') form?: ElementRef<HTMLFormElement>; 
+
+  onSubmit (title: string, ticketText: String)
+  {
+    console.log(title);
+    console.log(ticketText);
+    // to celar input elements nakon upisivanja
+    this.form?.nativeElement.reset();
   }
 }
