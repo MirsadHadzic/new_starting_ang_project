efde9966dceee4470b8ed3c24c4b792ba97d577c
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-05-12 21:27:50 +0200

    CONTENCT CHILD FCNTN

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ContentChild, contentChildren, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -21,10 +21,17 @@ export class ControlComponent {
   label = input.required<string>();
   private el = inject(ElementRef); 
   // @HostListener('click')
+  // upitnik je da moze biti undefined ako nicega nema
+//  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+//  private control = contentChildren<ElementRef>('input');
+private control = 
+   contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
 
   onClick() 
   {
     console.log('Clicked!');
     console.log(this.el);
+    console.log(this.control);
   }
 }
