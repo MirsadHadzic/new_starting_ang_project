91b72daa711417cbb7e1f99fd62e7ff59c9d71fa
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2025-05-13 12:25:39 +0200

    added a signals and revisited them for the detials part

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  detailsVisible = signal(false);
 
+
+  onToggleDetails()
+  {
+    // ! je stavljen da bi bilo suprotno, ako je bilo false bit ce true i vice versa
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
 }
